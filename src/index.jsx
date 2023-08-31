855525aeff09313f2d6ba78c9701721c71d4e7c6
rliu6915 <rliu6915@connect.hku.hk>
2023-08-31 13:00:03 +0800

    11.9/style

--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import './styles.css'
 
+// eslint-disable-next-line react/no-deprecated
 ReactDOM.render(<App />, document.getElementById('app'))
-
+// ReactDOM.createRoot(document.getElementById('app')).render(<App />)
